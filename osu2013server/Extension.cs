0f27a8d93623a2c094aaa23b4ddb48dcb224004e
jeesusmies <46649783+jeesusmies@users.noreply.github.com>
2020-12-29 15:02:22 +0200

    Added Extension.cs and Listener.cs

---
+++ osu2013server/Extension.cs
@@ -0,0 +1,10 @@
+﻿namespace osu2013server
+{
+    public static class Extension
+    {
+        public static void Log(dynamic obj, string message, LogStatus status)
+        {
+            
+        }
+    }
+}
---
+++ osu2013server/Listener.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Dynamic;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace osu2013server
+{
+    public class Listener
+    {
+        private string Prefix { get; set; }
+        private static HttpListener HttpListener { get; set; }
+
+        public Listener()
+        {
+            HttpListener = new();
+            HttpListener.Prefixes.Add(Prefix);
+        }
+
+        public void Run()
+        {
+            Task.Run(() =>
+            {
+                try
+                {
+                    HttpListener.Start();
+                }
+                catch (HttpListenerException hlex) when (hlex.ErrorCode == 32 || hlex.ErrorCode == 183)
+                {
+                    Console.Error.WriteLine("The desired port for the Bancho is in use.");
+                    return;
+                }
+                
+                Extension.Log();
+            });
+        }
+    }
+}
