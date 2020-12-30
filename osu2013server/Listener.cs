12d6d083c62a3be93b1050977ee8ea3f8d061f36
jeesusmies <46649783+jeesusmies@users.noreply.github.com>
2020-12-30 18:09:48 +0200

    added logging when requested

--- osu2013server/Listener.cs
+++ osu2013server/Listener.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Net;
 using System.Reflection;
 using System.Security.Cryptography;
+using System.Threading;
 using System.Threading.Tasks;
 using osu2013server.Attributes;
 using osu2013server.Enums;
@@ -59,6 +60,28 @@ namespace osu2013server
         public async Task ProcessContextAsync(HttpListenerContext ctx)
         {
             Stopwatch stopwatch = new();
+            stopwatch.Start();
+
+            var request = ctx.Request;
+            
+            /*
+            Console.WriteLine("URL: {0}", request.Url.OriginalString);
+            Console.WriteLine("Raw URL: {0}", request.RawUrl);
+            Console.WriteLine("Query: {0}", request.QueryString);
+
+            // Display the referring URI.
+            Console.WriteLine("Referred by: {0}", request.UrlReferrer);
+
+            //Display the HTTP method.
+            Console.WriteLine("HTTP Method: {0}", request.HttpMethod);
+            //Display the host information specified by the client;
+            Console.WriteLine("Host name: {0}", request.UserHostName);
+            Console.WriteLine("Host address: {0}", request.UserHostAddress);
+            Console.WriteLine("User agent: {0}", request.UserAgent);
+            */
+            
+            stopwatch.Stop();
+            Extension.Log(this, $@"Request to [{request.RawUrl}] took {stopwatch.Elapsed.Milliseconds}ms", LogStatus.Warning);
         }
 
         private void AddListeningRoutes()
