2dfd86e1301eda1d49ca93eb00ac365954d2e5e4
jeesusmies <46649783+jeesusmies@users.noreply.github.com>
2021-01-05 12:21:17 +0200

    Added mods into Mods.cs

--- osu2013server/Enums/Mods.cs
+++ osu2013server/Enums/Mods.cs
@@ -1,7 +1,24 @@
-﻿namespace osu2013server.Enums
+﻿using System;
+
+namespace osu2013server.Enums
 {
+    [Flags]
     public enum Mods
     {
-        
+        None = 0,
+        NoFail = 1,
+        Easy = 2,
+        NoVideo = 4,
+        Hidden = 8,
+        HardRock = 16,
+        SuddenDeath = 32,
+        DoubleTime = 64,
+        Relax = 128,
+        HalfTime = 256,
+        Flashlight = 1024,
+        Autoplay = 2048,
+        SpunOut = 4096,
+        Relax2 = 8192,
+        LastMod = 16384
     }
 }
